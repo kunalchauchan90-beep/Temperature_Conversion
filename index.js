44eca64c57035c71248ce9f0471fa082eb836df0
Kunal Chauhan <kunalchauchan.90@gmail.com>
2026-04-10 14:31:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const textbox=document.getElementById("textbox");
+const toferenheit=document.getElementById("tofehrenheit");
+const tocelsius=document.getElementById("tocelsius");
+
+const result=document.getElementById("result");
+let temp;
+
+function convert(){
+    if(tofehrenheit.checked){
+        temp=Number(textbox.value);
+        temp=temp * 9 / 5 + 32;
+        result.textContent = temp.toFixed(1) + "°F";
+    }
+    else if(tocelsius.checked){
+         temp=Number(textbox.value);
+        temp=(temp-32) * (5/9);
+        result.textContent = temp.toFixed(1) + "°C";
+    }
+    else{
+        result.textContent="select a unit";
+    }
+}
